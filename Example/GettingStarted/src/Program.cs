44a90b1342c46bfaffddabb0ba53c78a4a2dff80
Josh Gough <jsgough@gmail.com>
2012-10-26 03:10:36 -0300

    Added instructions (test force push hook notification)

--- Example/GettingStarted/src/Program.cs
+++ Example/GettingStarted/src/Program.cs
@@ -5,6 +5,7 @@ namespace GettingStarted
 {
     public class Program
     {
+        // Modify to have your own url and credentials here...
         private const string BaseUrl = "http://localhost/VersionOne.Web";
         private const string UserName = "admin";
         private const string Password = "admin";
