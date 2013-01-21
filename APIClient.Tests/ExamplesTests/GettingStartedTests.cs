0d52ddc2f534becb3307e28f7eaa70123ff5bf9d
codeEmitter <greg.johnson@versionone.com>
2013-01-21 09:53:07 -0500

    Example class and test setup.

---
+++ APIClient.Tests/ExamplesTests/GettingStartedTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace VersionOne.SDK.APIClient.Tests.ExamplesTests
+{
+    [TestFixture]
+    public class GettingStartedTests
+    {
+
+        private EnvironmentContext _context;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _context = new EnvironmentContext();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context = null;
+        }
+
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ APIClient/Examples/GettingStarted.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VersionOne.SDK.APIClient.Examples
+{
+    class GettingStarted
+    {
+
+        private readonly EnvironmentContext _context;
+
+        private IMetaModel _metaModel;
+        private IMetaModel _metaModelWithProxy;
+        private IServices _services;
+        private IServices _servicesWithProxy;
+        private IV1Configuration _config;
+
+        public GettingStarted()
+        {
+
+            _context = new EnvironmentContext();
+
+            _metaModel = _context.MetaModel;
+            _metaModelWithProxy = _context.MetaModelWithProxy;  //if you use a proxy server
+            _services = _context.Services;
+            _servicesWithProxy = _context.ServicesWithProxy;  //if you use a proxy server
+            _config = _context.V1Configuration;
+
+        }
+
+    }
+}
