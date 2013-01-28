b2eaa24aeae5172f8ebb182af45fcd41e8d2ff91
Joe Koberg <joe.koberg@versionone.com>
2013-01-28 11:46:29 -0500

    Revert "A little cleanup from the Documentation branch merge to remove references to the AssemblyInfo file(s) from the project files since the files are non-existent."
    This reverts commit f887af21162347612feb0fa1efb7fa29010a4e6f.

--- APIClient/Examples/GettingStarted.cs
+++ APIClient/Examples/GettingStarted.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
