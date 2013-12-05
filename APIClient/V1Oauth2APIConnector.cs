592cbf139c02bbc4760b3b07c09ccd54e1e36988
Joe Koberg <joe.koberg@versionone.com>
2013-12-05 18:01:27 -0500

    Defer to OAuth2Client for Bearer handling

--- APIClient/V1Oauth2APIConnector.cs
+++ APIClient/V1Oauth2APIConnector.cs
@@ -14,10 +14,8 @@ namespace VersionOne.SDK.APIClient
 	public class V1OAuth2APIConnector : IAPIConnector
 	{
 		private readonly string _urlPrefix;
-		private readonly IStorage _storage;
 		private CookieContainer _cookieContainer;
-		private OAuth2Client.Credentials _creds;
-		private OAuth2Client.Secrets _secrets;
+		private readonly OAuth2Credentials _creds;
 		private string _callerUserAgent = "";
 
 		private const string EndpointScope="apiv1";
@@ -38,22 +36,17 @@ namespace VersionOne.SDK.APIClient
 		public V1OAuth2APIConnector(string urlPrefix, IStorage storage = null, ProxyProvider proxy = null)
 		{
 			_urlPrefix = urlPrefix;
-			_storage = storage ?? OAuth2Client.Storage.JsonFileStorage.Default;
 			_proxyProvider = proxy;
-			ReadSecretsAndCreds();
+			AuthenticationManager.Unregister("basic");
+			AuthenticationManager.Register(new OAuth2BearerModule());
+			var mystorage =
+				new Microsoft.FSharp.Core.FSharpOption<IStorage>(storage ?? OAuth2Client.Storage.JsonFileStorage.Default);
+			var myproxy = _proxyProvider != null
+				              ? new Microsoft.FSharp.Core.FSharpOption<IWebProxy>(_proxyProvider.CreateWebProxy())
+				              : Microsoft.FSharp.Core.FSharpOption<IWebProxy>.None;
+			_creds = new OAuth2Credentials(EndpointScope, mystorage, myproxy);
 		}
 
-		private void ReadSecretsAndCreds()
-		{
-			_secrets = _storage.GetSecrets();
-			_creds = _storage.GetCredentials();
-		}
-
-		private void AddBearer(HttpWebRequest request)
-		{
-			request.Headers["Authorization"] =
-				new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _creds.AccessToken).ToString();
-		}
 
 		public static System.Reflection.AssemblyName MyAssemblyName =
 			System.Reflection.Assembly.GetAssembly(typeof(V1APIConnector)).GetName();
@@ -62,15 +55,18 @@ namespace VersionOne.SDK.APIClient
 		{
 			get
 			{
-				return String.Format("{0}/{1} ({2}, client_id={3}) {4}", MyAssemblyName.Name, MyAssemblyName.Version,
-												MyAssemblyName.FullName, _secrets.client_id, _callerUserAgent);
+				return String.Format("{0}/{1} ({2}) {3}", MyAssemblyName.Name, MyAssemblyName.Version,
+												MyAssemblyName.FullName, _callerUserAgent);
 			}
 		}
 
 		private HttpWebRequest CreateRequest(string url)
 		{
 			var request = (HttpWebRequest)WebRequest.Create(url);
-			AddBearer(request);
+			request.Credentials = _creds;
+			request.PreAuthenticate = true;
+			request.AllowAutoRedirect = true;
+
 			if (_proxyProvider != null)
 			{
 				request.Proxy = _proxyProvider.CreateWebProxy();
@@ -94,36 +90,21 @@ namespace VersionOne.SDK.APIClient
 			var req = CreateRequest(url);
 			req.ContentType = contentType;
 			req.Method = "GET";
-			try
+			var resp = req.GetResponse();
+			if (Config.IsDebugMode)
 			{
-				var resp = req.GetResponse();
-				if (Config.IsDebugMode)
-				{
-					Debug.WriteLine(string.Empty);
-					Debug.WriteLine("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv");
-					Debug.WriteLine("Get....");
-					Debug.WriteLine("URL: " + url);
-					Debug.WriteLine("Response from: " + resp.ResponseUri);
-					Debug.WriteLine(resp.Headers.ToString());
-					Debug.WriteLine(resp.ToString());
-					Debug.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-					Debug.WriteLine(string.Empty);
-				}
-				return resp.GetResponseStream();
+				Debug.WriteLine(string.Empty);
+				Debug.WriteLine("vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv");
+				Debug.WriteLine("Get....");
+				Debug.WriteLine("URL: " + url);
+				Debug.WriteLine("Response from: " + resp.ResponseUri);
+				Debug.WriteLine(resp.Headers.ToString());
+				Debug.WriteLine(resp.ToString());
+				Debug.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+				Debug.WriteLine(string.Empty);
 			}
-			catch (WebException ex)
-			{
-				var resp = (HttpWebResponse)ex.Response;
-				if (refreshTokenIfNeeded && ex.Status == WebExceptionStatus.ProtocolError && resp.StatusCode == HttpStatusCode.Unauthorized)
-				{
-					var proxy = _proxyProvider != null ? _proxyProvider.CreateWebProxy() : null;
-					var authclient = new OAuth2Client.AuthClient(_secrets, EndpointScope, proxy, null);
-					_creds = authclient.refreshAuthCode(_creds);
-					_creds = _storage.StoreCredentials(_creds);
-					return HttpGet(apipath, refreshTokenIfNeeded: false);
-				}
-				throw;
-			}
+			return resp.GetResponseStream();
+			
 		}
 
 		public Stream HttpPost(string apipath, byte[] body, bool refreshTokenIfNeeded = true, string contentType = "text/xml")
@@ -143,78 +124,10 @@ namespace VersionOne.SDK.APIClient
 				Debug.WriteLine("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 				Debug.WriteLine("");
 			}
-			try
-			{
-				req.ContentLength = body.Length;
-				req.GetRequestStream().Write(body, 0, body.Length);
-				var resp = req.GetResponse();
-				return resp.GetResponseStream();
-			}
-			catch (WebException ex)
-			{
-				var resp = (HttpWebResponse)ex.Response;
-				if (refreshTokenIfNeeded && ShouldRefresh(ex))
-				{
-					var proxy = _proxyProvider != null ? _proxyProvider.CreateWebProxy() : null;
-					var authclient = new OAuth2Client.AuthClient(_secrets, EndpointScope, proxy, null);
-					_creds = authclient.refreshAuthCode(_creds);
-					_creds = _storage.StoreCredentials(_creds);
-					return HttpPost(apipath, body, refreshTokenIfNeeded: false);
-				}
-				throw;
-			}
-		}
-
-		private List<HttpStatusCode> CodesToRefresh = new List<HttpStatusCode>
-			{
-				HttpStatusCode.BadRequest,
-				HttpStatusCode.Forbidden,
-				HttpStatusCode.MethodNotAllowed,
-				HttpStatusCode.Unauthorized
-			};
-
-		private bool isV1WithBrokenBearer(HttpWebResponse response)
-		{
-			var v1 = response.Headers["VersionOne"];
-			if (string.IsNullOrEmpty(v1)) return false;
-			var re = new System.Text.RegularExpressions.Regex(@"(.*)/(\d+).(\d+).(\d+).(\d+); (.*)");
-			var match = re.Match(v1);
-			if (match.Success)
-			{
-				var edition = match.Groups[0].Value;
-				var major = Int32.Parse(match.Groups[1].Value);
-				var minor = Int32.Parse(match.Groups[2].Value);
-				var release = Int32.Parse(match.Groups[3].Value);
-				var patch = Int32.Parse(match.Groups[4].Value);
-				var lexicon = match.Groups[4].Value;
-				if (major < 13) return true;
-				if (major == 13 && minor <= 3) return true;
-			}
-			return false;
-		}
-
-		private bool ShouldRefresh(WebException ex)
-		{
-			var response = ex.Response as HttpWebResponse;
-			if (response != null)
-			{
-				if (isV1WithBrokenBearer(response))
-				{
-					if (CodesToRefresh.Contains(response.StatusCode))
-					{
-						return true;
-					}
-				}
-				else
-				{
-					if (CodesToRefresh.Contains(response.StatusCode))
-					{
-						var h = response.GetResponseHeader("WWW-Authenticate");
-						return h.Contains("Bearer") && h.Contains("invalid_token");
-					}
-				}
-			}
-			return false;
+			req.ContentLength = body.Length;
+			req.GetRequestStream().Write(body, 0, body.Length);
+			var resp = req.GetResponse();
+			return resp.GetResponseStream();
 		}
 
 		public Stream GetData()
