7cd56a72dabdf67db5a25ee9b694283a4ae7d8b7
Josh Gough <jsgough@gmail.com>
2013-12-06 18:01:35 -0500

    - Upgraded build.sh to common build tools - Corrected allowedVersions ranges in packages.config files

--- build.sh
+++ build.sh
@@ -1,115 +1,11 @@
 #!/bin/bash -e
 
-set -e
-set -x
-
-
 # ----- Variables -------------------------------------------------------------
 # Variables in the build.properties file will be available to Jenkins
 # build steps. Variables local to this script can be defined below.
 . ./build.properties
-
-
-
-# -----------------------------------------------------------------------------
-
-# fix for jenkins inserting the windows-style path in $WORKSPACE
-cd "$WORKSPACE"
-export WORKSPACE=`pwd`
-
-
-
-# ----- Utility functions -----------------------------------------------------
-
-function winpath() {
-  # Convert gitbash style path '/c/Users/Big John/Development' to 'c:\Users\Big John\Development',
-  # via dumb substitution. Handles drive letters; incurs process creation penalty for sed.
-  if [ -e /etc/bash.bashrc ] ; then
-    # Cygwin specific settings
-    echo "`cygpath -w $1`"
-  else
-    # Msysgit specific settings
-    echo "$1" | sed -e 's|^/\(\w\)/|\1:\\|g;s|/|\\|g'
-  fi
-}
-
-function bashpath() {
-  # Convert windows style path 'c:\Users\Big John\Development' to '/c/Users/Big John/Development'
-  # via dumb substitution. Handles drive letters; incurs process creation penalty for sed.
-  if [ -e /etc/bash.bashrc ] ; then
-    # Cygwin specific settings
-    echo "`cygpath $1`"
-  else
-    # Msysgit specific settings
-    echo "$1" | sed -e 's|\(\w\):|/\1|g;s|\\|/|g'
-  fi
-}
-
-function parentwith() {  # used to find $WORKSPACE, below.
-  # Starting at the current dir and progressing up the ancestors,
-  # retuns the first dir containing $1. If not found returns pwd.
-  SEARCHTERM="$1"
-  DIR=`pwd`
-  while [ ! -e "$DIR/$SEARCHTERM" ]; do
-    NEWDIR=`dirname "$DIR"`
-    if [ "$NEWDIR" = "$DIR" ]; then
-      pwd
-      return
-    fi
-    DIR="$NEWDIR"
-  done
-  echo "$DIR"
-  }
-
-
-# If we aren't running under jenkins. some variables will be unset.
-# So set them to a reasonable value
-
-if [ -z "$WORKSPACE" ]; then
-  export WORKSPACE=`parentwith .git`;
-fi
-
-TOOLSDIRS=". $WORKSPACE/GetBuildTools $WORKSPACE/v1_build_tools $WORKSPACE/../v1_build_tools"
-#TOOLSDIRS="."
-for D in $TOOLSDIRS; do
-  if [ -d "$D/bin" ]; then
-    export BUILDTOOLS_PATH="$D/bin"
-  fi
-done
-
-if [ -z "$DOTNET_PATH" ]; then
-  for D in `bashpath "$SYSTEMROOT\\Microsoft.NET\\Framework\\v*"`; do
-    if [ -d $D ]; then
-      export DOTNET_PATH="$D"
-    fi
-  done
-fi
-echo "Using $DOTNET_PATH for .NET"
-
-export PATH="$PATH:$BUILDTOOLS_PATH:$DOTNET_PATH"
-
-if [ -z "$SIGNING_KEY_DIR" ]; then
-  export SIGNING_KEY_DIR=`pwd`;
-fi
-
-export SIGNING_KEY="$SIGNING_KEY_DIR/VersionOne.snk"
-
-if [ -f "$SIGNING_KEY" ]; then 
-  export SIGN_ASSEMBLY="true"
-else
-  export SIGN_ASSEMBLY="false"
-  echo "Please place VersionOne.snk in `pwd` or $SIGNING_KEY_DIR to enable signing.";
-fi
-
-if [ -z "$VERSION_NUMBER" ]; then
-  export VERSION_NUMBER="0.0.0"
-fi
-
-if [ -z "$BUILD_NUMBER" ]; then
-  # presume local workstation, use date-based build number
-  export BUILD_NUMBER=`date +%H%M`  # hour + minute
-fi
-
+if [ -d build-tools ]; then cd build-tools && git fetch && git stash && git pull && cd ..; else git clone https://github.com/versionone/openAgile-build-tools.git build-tools; fi
+source ./build-tools/common.sh
 
 
 # ---- Produce .NET Metadata --------------------------------------------------
@@ -145,7 +41,7 @@ done
 rm -rf $WORKSPACE/$MAIN_DIR/*.nupkg
 MSBuild.exe $SOLUTION_FILE -m -t:Clean
 
-
+nuget_packages_refresh
 
 # ---- Build solution using msbuild -------------------------------------------
 
@@ -200,5 +96,3 @@ fi
 cd $WORKSPACE/$MAIN_DIR
 $WORKSPACE/.nuget/NuGet.exe pack $MAIN_CSPROJ -Symbols -prop Configuration=$Configuration
 cd $WORKSPACE
-
-
