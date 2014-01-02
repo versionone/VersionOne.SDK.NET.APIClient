5fe127d1dcff097438cb7a26069a25c4a9a55ce9
Josh Gough <jsgough@gmail.com>
2014-01-02 16:40:16 -0500

    Added simple test outline for ApiConnector using BasicCredentialProvider

---
+++ APIClient.Tests/Authentication/BasicCredentialProviderTester.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using VersionOne.SDK.APIClient.Authentication;
+
+namespace VersionOne.SDK.APIClient.Tests.Authentication {
+	[TestFixture]
+	[Ignore("This test shows how to use the ApiConnector")]
+	public class BasicCredentialProviderTester {
+		private const string V1Path = "http://localhost/VersionOne.Web/";
+		private const string V1Username = "admin";
+		private const string V1Password = "admin";
+
+		[Test]
+		public void BasicCredential_test()
+		{
+			var connector = new ApiConnector(V1Path,
+				new BasicCredentialProvider(V1Username, V1Password),
+				new WindowsIntegratedCredentialProvider()
+				);
+			connector.BeginRequest("blah");
+		}
+	}
+}
--- APIClient/Authentication/ApiConnector.cs
+++ APIClient/Authentication/ApiConnector.cs
@@ -4,14 +4,15 @@ using System.Net;
 
 namespace VersionOne.SDK.APIClient.Authentication
 {
-    public class ApiConnector : V1CredsAPIConnector
-    {
-        public ApiConnector(string urlPrefix, ICredentialProvider authHandler)
-            : base(urlPrefix)
-        {
-            authHandler.Handle(new Uri(urlPrefix), Credentials as CredentialCache, GetProxyProvider());
-        }
-
-
-    }
+	public class ApiConnector : V1CredsAPIConnector
+	{
+		public ApiConnector(string urlPrefix, params ICredentialProvider[] credentialProviders)
+			: base(urlPrefix)
+		{
+			foreach (var credentialProvider in credentialProviders)
+			{
+				credentialProvider.Handle(new Uri(urlPrefix), Credentials as CredentialCache, GetProxyProvider());
+			}
+		}
+	}
 }
