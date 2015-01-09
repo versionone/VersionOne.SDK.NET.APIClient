8c2f31463c52454864466220ec078b453ae12ab6
pablofreiberg <pablo.freiberg@gmail.com>
2015-01-09 17:30:14 -0300

    S-50869 Created new project containing integration tests switched to use MSTest

---
+++ APIClient.Integration.Tests/ExamplesTests/EnvironmentContextTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VersionOne.SDK.APIClient.Integration.Tests.ExamplesTests
+{
+    [TestClass]
+    public class EnvironmentContextTest
+    {
+        /// <summary>
+        /// This is the simplest scenario, it works with the configuration from app.config
+        /// </summary>
+        [TestMethod]
+        public void EnvironmentContextUsageTest()
+        {
+            var context = new EnvironmentContext();
+
+            var memberId = Oid.FromToken("Member:20", context.MetaModel);
+            var query = new Query(memberId);
+            var nameAttribute = context.MetaModel.GetAttributeDefinition("Member.Name");
+            var emailAttribute = context.MetaModel.GetAttributeDefinition("Member.Email");
+
+            query.Selection.Add(nameAttribute);
+            query.Selection.Add(emailAttribute);
+
+            var result = context.Services.Retrieve(query);
+            var member = result.Assets[0];
+
+            Assert.AreEqual("Administrator", member.GetAttribute(nameAttribute).Value);
+            Assert.AreEqual("versionone@mailinator.com", member.GetAttribute(emailAttribute).Value);
+        }
+    }
+}
---
+++ APIClient.Integration.Tests/ProxyTester.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VersionOne.SDK.APIClient.Integration.Tests
+{
+    [TestClass]
+    [Ignore]
+    public class ProxyTester
+    {
+        private const string V1Path = "http://integsrv01.internal.corp/VersionOneTest/";
+        private const string V1Username = "admin";
+        private const string V1Password = "admin";
+
+        private const string ProxyPath = "http://integvm01:3128";
+        private const string ProxyUsername = "user1";
+        private const string ProxyPassword = "user1";
+
+        [TestMethod]
+        public void QueryProjects()
+        {
+            var proxyProvider = new ProxyProvider(new Uri(ProxyPath), ProxyUsername, ProxyPassword);
+
+            IAPIConnector metaConnector = new VersionOneAPIConnector(V1Path + "meta.v1/", proxyProvider: proxyProvider)
+            .WithVersionOneUsernameAndPassword(V1Username, V1Password);
+            IMetaModel metaModel = new MetaModel(metaConnector);
+
+            IAPIConnector dataConnector = new VersionOneAPIConnector(V1Path + "rest-1.v1/", proxyProvider: proxyProvider)
+            .WithVersionOneUsernameAndPassword(V1Username, V1Password);
+            IServices services = new Services(metaModel, dataConnector);
+
+            var projectType = metaModel.GetAssetType("Scope");
+            var scopeQuery = new Query(projectType);
+            var result = services.Retrieve(scopeQuery);
+
+            Assert.IsTrue(result.Assets.Count > 0);
+        }
+    }
+}
