8fdb9ac9c34d1cc6d13df3016a38f9eea49803c3
pablofreiberg <pablo.freiberg@gmail.com>
2015-01-09 17:30:14 -0300

    S-50869 Created new project containing integration tests switched to use MSTest

---
+++ APIClient.Integration.Tests/ExamplesTests/VersionOneAPIConnectorExamples.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VersionOne.SDK.APIClient.Integration.Tests.ExamplesTests
+{
+    [TestClass]
+    public class VersionOneAPIConnectorExamples
+    {
+        private string _username;
+        private string _password;
+        private string _prefix;
+        private const string Path = "rest-1.v1/Data/Member/20";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _username = ConfigurationManager.AppSettings["V1UserName"];
+            _password = ConfigurationManager.AppSettings["V1Password"];
+            _prefix = ConfigurationManager.AppSettings["V1Url"];
+        }
+
+
+        /// <summary>
+        /// This is the simplest scenario, you just need to provide the UserName and Password,
+        /// where Prefix is the Url for your V1 instance, e.g. "http://localhost/VersionOne.Web/"
+        /// </summary>
+        [TestMethod]
+        public void basic_authentication_with_username_and_password()
+        {
+            // creating and configuring the connector
+            var connector = new VersionOneAPIConnector(_prefix)
+                .WithVersionOneUsernameAndPassword(_username, _password);
+
+            // requesting some data that requires authentication
+            using (connector.GetData(Path)) ;
+        }
+
+        [TestMethod]
+        public void basic_authentication_with_constructor_supplied_credential()
+        {
+            var simpleCred = new NetworkCredential(_username, _password);
+            var connector = new VersionOneAPIConnector(_prefix, simpleCred);
+
+            using (connector.GetData(Path)) ;
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void windows_integrated_authentication()
+        {
+            var connector = new VersionOneAPIConnector(_prefix)
+                .WithWindowsIntegratedAuthentication();
+
+            using (connector.GetData(Path)) ;
+        }
+
+        [TestMethod]
+        public void fluent_configuration_with_multiple_credentials()
+        {
+            var connector = new VersionOneAPIConnector(_prefix)
+                .WithVersionOneUsernameAndPassword(_username, _password)
+                .WithWindowsIntegratedAuthentication();
+
+            using (connector.GetData(Path)) ;
+        }
+
+        [TestMethod]
+        public void multiple_credentials_via_user_supplied_credential_cache()
+        {
+            var simpleCred = new NetworkCredential(_username, _password);
+
+            var windowsIntegratedCredential = CredentialCache.DefaultNetworkCredentials;
+
+            var cache = new CredentialCache();
+            var uri = new Uri(_prefix);
+            cache.Add(uri, "Basic", simpleCred);
+            // Suppose for some weird reason you just wanted to support NTLM:
+            cache.Add(uri, "NTLM", windowsIntegratedCredential);
+
+            var connector = new VersionOneAPIConnector(_prefix, cache);
+            using (connector.GetData(Path)) ;
+        }
+
+        [TestMethod]
+        public void full_working_example_with_basic_authentication()
+        {
+            // Setting up a basic connection with the connector and retrieving the Member with the ID 20
+            var dataConnector = new VersionOneAPIConnector(_prefix + "rest-1.v1/")
+                .WithVersionOneUsernameAndPassword(_username, _password);
+            var metaConnector = new VersionOneAPIConnector(_prefix + "meta.v1/");
+
+            IMetaModel metaModel = new MetaModel(metaConnector);
+            IServices services = new Services(metaModel, dataConnector);
+
+            var memberId = Oid.FromToken("Member:20", metaModel);
+            var query = new Query(memberId);
+            var memberName = metaModel.GetAttributeDefinition("Member.Name");
+            query.Selection.Add(memberName);
+
+            var result = services.Retrieve(query);
+        }
+    }
+}
---
+++ APIClient.Integration.Tests/QueryTests/QueryBuilderTests/FindBuilderTester.cs
@@ -0,0 +1,60 @@
+﻿using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VersionOne.SDK.APIClient.Integration.Tests.QueryTests.QueryBuilderTests
+{
+    [TestClass]
+    public class FindBuilderTester
+    {
+        private FindBuilder _target;
+        private IMetaModel _metaModel;
+
+        [TestInitialize]
+        public void TestFixtureSetup()
+        {
+            _target = new FindBuilder();
+            string metaUrl = ConfigurationManager.AppSettings["V1Url"] + "meta.v1/";
+            var metaConnector = new VersionOneAPIConnector(metaUrl);
+            _metaModel = new MetaModel(metaConnector);
+        }
+
+        [TestMethod]
+        public void BuildWithInvalidParametersTest()
+        {
+            var assetType = _metaModel.GetAssetType("Member");
+            _target.Build(null, null);
+            _target.Build(new Query(assetType), new BuildResult());
+            var result = new BuildResult();
+            _target.Build(new Query(assetType), result);
+            Assert.AreEqual(0, result.QuerystringParts.Count);
+        }
+
+        [TestMethod]
+        public void BuildWithValidParametersTest()
+        {
+            var assetType = _metaModel.GetAssetType("Member");
+            var query = new Query(assetType);
+            var nameAttribute = assetType.GetAttributeDefinition("Username");
+            query.Selection.Add(nameAttribute);
+            query.Find = new QueryFind("admin", new AttributeSelection("Username", assetType));
+            var result = new BuildResult();
+            _target.Build(query, result);
+            Assert.AreEqual(2, result.QuerystringParts.Count); //one part for find, one part for findin
+            Assert.AreEqual("?find=admin&findin=Member.Username", result.ToUrl());
+        }
+
+        [TestMethod]
+        public void QueryStringEscapeTest()
+        {
+            var assetType = _metaModel.GetAssetType("Member");
+            var query = new Query(assetType);
+            var nameAttribute = assetType.GetAttributeDefinition("Username");
+            query.Selection.Add(nameAttribute);
+            query.Find = new QueryFind("admin@mydomain.com", new AttributeSelection("Username", assetType));  //make sure ampersand get url encoded
+            var result = new BuildResult();
+            _target.Build(query, result);
+            Assert.AreEqual(2, result.QuerystringParts.Count); //one part for find, one part for findin
+            Assert.AreEqual("?find=admin%40mydomain.com&findin=Member.Username", result.ToUrl());
+        }
+    }
+}
