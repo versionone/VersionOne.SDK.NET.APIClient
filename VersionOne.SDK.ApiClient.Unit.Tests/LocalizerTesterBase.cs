2b488a81914984c144891f79ab9a1a5566a0c0fe
pablofreiberg <pablo.freiberg@gmail.com>
2015-01-09 13:13:59 -0300

    S-50869 Created new project containing unit tests switched to use MSTest

---
+++ VersionOne.SDK.ApiClient.Unit.Tests/LocalizerTesterBase.cs
@@ -0,0 +1,20 @@
+﻿using VersionOne.SDK.APIClient;
+
+namespace VersionOne.SDK.ApiClient.Unit.Tests
+{
+    public class LocalizerTesterBase
+    {
+        private ILocalizer _loc;
+        protected ILocalizer Loc
+        {
+            get
+            {
+                if (_loc == null)
+                    _loc = new Localizer(new TextResponseConnector("TestData.xml", "loc.v1/", LocTestKeys));
+                return _loc;
+            }
+        }
+
+        protected virtual string LocTestKeys { get { return null; } }
+    }
+}
