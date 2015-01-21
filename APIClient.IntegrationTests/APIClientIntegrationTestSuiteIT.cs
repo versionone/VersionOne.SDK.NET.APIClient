5b395ad4261d4f504914a61fcf902b0fad896155
pablofreiberg <pablo.freiberg@gmail.com>
2015-01-21 10:40:38 -0300

    no message

---
+++ APIClient.IntegrationTests/APIClientIntegrationTestSuiteIT.cs
@@ -0,0 +1,61 @@
+﻿namespace VersionOne.SDK.APIClient.IntegrationTests
+{
+    public class APIClientIntegrationTestSuiteIT
+    {
+        private static IMetaModel _metaModel;
+        private static IServices _services;
+        private static EnvironmentContext _context;
+        private static Oid _projectId;
+
+        const string TestProjectName = ".Net.SDK Integration Tests";
+
+        public static Oid ProjectId
+        {
+            get
+            {
+                if (_projectId == null)
+                {
+                    Setup();
+                }
+
+                return _projectId;
+            }
+        }
+
+        public static EnvironmentContext Context
+        {
+            get
+            {
+                if (_context == null)
+                {
+                    Setup();
+                }
+
+                return _context;
+            }
+        }
+
+        public static void Setup()
+        {
+            _context = new EnvironmentContext();
+            _metaModel = _context.MetaModel;
+            _services = _context.Services;
+
+            CreateTestProject();
+        }
+        
+        /// <summary>
+        /// Creates a new V1 project for integration test assets.
+        /// </summary>
+        private static void CreateTestProject()
+        {
+            var assetType = _metaModel.GetAssetType("Scope");
+            var nameAttribute = assetType.GetAttributeDefinition("Name");
+            var projectId = Oid.FromToken("Scope:0", _metaModel);
+            var newAsset = _services.New(assetType, projectId);
+            newAsset.SetAttributeValue(nameAttribute, TestProjectName);
+            _services.Save(newAsset);
+            _projectId = newAsset.Oid.Momentless;
+        }
+    }
+}
---
+++ APIClient.IntegrationTests/ConfigurationTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VersionOne.SDK.APIClient.IntegrationTests
+{
+    [TestClass]
+    public class ConfigurationTests
+    {
+        [TestMethod]
+        public void TestConfig()
+        {
+            var v1Config = APIClientIntegrationTestSuitIT.Context.V1Configuration;
+
+            Assert.IsTrue(v1Config.EffortTracking);
+        }
+    }
+}
