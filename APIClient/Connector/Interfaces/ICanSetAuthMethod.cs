c0de5f4f1df0178a95e3e45ed193aba83536e155
pablofreiberg <pablo.freiberg@gmail.com>
2015-03-12 10:36:03 -0300

    Added xml comments to connector interfaces

--- APIClient/Connector/Interfaces/ICanSetAuthMethod.cs
+++ APIClient/Connector/Interfaces/ICanSetAuthMethod.cs
@@ -2,8 +2,19 @@
 {
     public interface ICanSetAuthMethodOrApi : ICanSetApi
     {
+        /// <summary>
+        /// Authenticates using username and password. 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
         ICanSetProxyOrApi WithUsernameAndPassword(string username, string password);
 
+        /// <summary>
+        /// Authenticates using access token.
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <returns></returns>
         ICanSetProxyOrApi WithAccessToken(string accessToken);
     }
 }
