cb94a27677d435b7255d08df213c604e7a2027f9
pablofreiberg <pablo.freiberg@gmail.com>
2015-04-10 13:21:17 -0300

    Ignored attachment tests

--- APIClient.IntegrationTests/IntegrationTests.cs
+++ APIClient.IntegrationTests/IntegrationTests.cs
@@ -128,11 +128,12 @@ namespace VersionOne.SDK.APIClient.IntegrationTests
             Assert.AreEqual(1, story.GetAttribute(childrenAttribute).Values.Cast<object>().Count());
         }
 
+        [Ignore]
         [TestMethod]
         public void CreateStoryWithAttachment()
         {
             var services = GetServices();
-            string file = "./versionone.png";
+            string file = "versionone.png";
 
             Assert.IsTrue(File.Exists(file));
             
@@ -271,11 +272,12 @@ namespace VersionOne.SDK.APIClient.IntegrationTests
             Assert.AreEqual(1, story.GetAttribute(childrenAttribute).Values.Cast<object>().Count());
         }
 
+        [Ignore]
         [TestMethod]
         public void CreateDefectWithAttachment()
         {
             var services = GetServices();
-            string file = "./versionone.png";
+            string file = "versionone.png";
 
             Assert.IsTrue(File.Exists(file));
 
