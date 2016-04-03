f16f39cb013921b7d6d9f254e81e4f9eb24ea957
jogoshugh <jsgough@gmail.com>
2016-04-03 00:23:11 -0400

    Reformatted FluentQuery

--- Example/GettingStarted/src/ApiVNext/FluentQuery.cs
+++ Example/GettingStarted/src/ApiVNext/FluentQuery.cs
@@ -5,137 +5,142 @@ using VersionOne.SDK.APIClient;
 
 namespace ApiVNext
 {
-    public class FluentQuery
-    {
-        public Query RawQuery { get; set; }
-        public string AssetTypeName { get; set; }
-        public List<Tuple<string, object, FilterTerm.Operator>> WhereCriteria { get; set; }
-        public List<object> SelectFields { get; set; }
-        public Action<IEnumerable<AssetClassBase>> OnSuccess { get; set; }
-        public Action OnEmptyResults { get; set; }
-        public Action<Exception> OnError { get; set; }
+	public class FluentQuery
+	{
+		public Query RawQuery { get; set; }
+		public string AssetTypeName { get; set; }
+		public List<Tuple<string, object, FilterTerm.Operator>> WhereCriteria { get; set; }
+		public List<object> SelectFields { get; set; }
+		public Action<IEnumerable<AssetClassBase>> OnSuccess { get; set; }
+		public Action OnEmptyResults { get; set; }
+		public Action<Exception> OnError { get; set; }
 
-        public FluentQuery(
-            string assetTypeName)
-        {
-            RawQuery = new Query(MetaModelProvider.Meta.GetAssetType(assetTypeName));
-            AssetTypeName = assetTypeName;
+		public FluentQuery(
+			string assetTypeName) : this(assetTypeName, MetaModelProvider.Meta)
+		{
+		}
 
-            WhereCriteria = new List<Tuple<string, object, FilterTerm.Operator>>();
-            SelectFields = new List<object>();
-        }
+		public FluentQuery(
+			string assetTypeName, IMetaModel metaModel)
+		{
+			RawQuery = new Query(metaModel.GetAssetType(assetTypeName));
+			AssetTypeName = assetTypeName;
 
-        public FluentQuery Where(params Tuple<string, object, FilterTerm.Operator>[] criteria)
-        {
-            WhereCriteria.AddRange(criteria);
+			WhereCriteria = new List<Tuple<string, object, FilterTerm.Operator>>();
+			SelectFields = new List<object>();
+		}
 
-            return this;
-        }
+		public FluentQuery Where(params Tuple<string, object, FilterTerm.Operator>[] criteria)
+		{
+			WhereCriteria.AddRange(criteria);
 
-        public FluentQuery Select(params object[] fields)
-        {
-            SelectFields.AddRange(fields);
+			return this;
+		}
 
-            return this;
-        }
+		public FluentQuery Select(params object[] fields)
+		{
+			SelectFields.AddRange(fields);
 
-        public FluentQuery Success(Action<IEnumerable<AssetClassBase>> callback)
-        {
-            OnSuccess = callback;
+			return this;
+		}
 
-            return this;
-        }
+		public FluentQuery Success(Action<IEnumerable<AssetClassBase>> callback)
+		{
+			OnSuccess = callback;
 
-        public FluentQuery WhenEmpty(Action callback)
-        {
-            OnEmptyResults = callback;
+			return this;
+		}
 
-            return this;
-        }
+		public FluentQuery WhenEmpty(Action callback)
+		{
+			OnEmptyResults = callback;
 
-        public FluentQuery Error(Action<Exception> callback)
-        {
-            OnError = callback;
+			return this;
+		}
 
-            return this;
-        }
+		public FluentQuery Error(Action<Exception> callback)
+		{
+			OnError = callback;
 
-        public FluentQuery Execute(Action<IEnumerable<AssetClassBase>> successCallback = null)
-        {
-            if (OnSuccess == null && successCallback == null)
-            {
-                throw new NullReferenceException("Must specify the OnSuccess callback before calling Execute or pass it directly to Execute as a parameter");
-            }
+			return this;
+		}
 
-            try
-            {
-                var attributes = new List<IAttributeDefinition>();
+		public FluentQuery Execute(Action<IEnumerable<AssetClassBase>> successCallback = null)
+		{
+			if (OnSuccess == null && successCallback == null)
+			{
+				throw new NullReferenceException("Must specify the OnSuccess callback before calling Execute or pass it directly to Execute as a parameter");
+			}
 
-                if (SelectFields.Count > 0)
-                {
-                    attributes.AddRange(
-                        SelectFields.Select(
-                            m => MetaModelProvider.Meta.GetAttributeDefinition(AssetTypeName
-                                + "." + m.ToString())));
-                }
-                RawQuery.Selection.AddRange(attributes);
+			try
+			{
+				var attributes = new List<IAttributeDefinition>();
 
-                if (WhereCriteria.Count > 0)
-                {
-                    var andTerms = new List<FilterTerm>();
+				if (SelectFields.Count > 0)
+				{
+					attributes.AddRange(
+						SelectFields.Select(
+							m => MetaModelProvider.Meta.GetAttributeDefinition(AssetTypeName
+								+ "." + m.ToString())));
+				}
+				RawQuery.Selection.AddRange(attributes);
 
-                    foreach (var tuple in WhereCriteria)
-                    {
-                        var attribute = MetaModelProvider.Meta.GetAttributeDefinition(AssetTypeName
-                                                                                      + "." + tuple.Item1);
-                        var item = tuple.Item2;
-                        var term = new FilterTerm(attribute);
-                        term.Operate(tuple.Item3, item);
-                        andTerms.Add(term);
-                    }
+				if (WhereCriteria.Count > 0)
+				{
+					var andTerms = new List<FilterTerm>();
 
-                    var andTerm = new AndFilterTerm(andTerms.ToArray());
-                    RawQuery.Filter = andTerm;
-                }
+					foreach (var tuple in WhereCriteria)
+					{
+						var attribute = MetaModelProvider.Meta.GetAttributeDefinition(AssetTypeName
+																					  + "." + tuple.Item1);
+						var item = tuple.Item2;
+						var term = new FilterTerm(attribute);
+						term.Operate(tuple.Item3, item);
+						andTerms.Add(term);
+					}
 
-                var list = new List<AssetClassBase>();
+					var andTerm = new AndFilterTerm(andTerms.ToArray());
+					RawQuery.Filter = andTerm;
+				}
 
-                var result = ServicesProvider.Services.Retrieve(RawQuery);
+				var list = new List<AssetClassBase>();
 
-                if (result.Assets.Count == 0)
-                {
-                    OnSuccess(list);
-                }
+				var result = ServicesProvider.Services.Retrieve(RawQuery);
 
-                list.AddRange(result.Assets.Select(a => new AssetClassBase(a, AssetTypeName)));
+				if (result.Assets.Count == 0)
+				{
+					OnSuccess?.Invoke(list);
+				}
 
-                if (list.Count == 0 && OnEmptyResults != null)
-                {
-                    OnEmptyResults();
-                }
+				list.AddRange(result.Assets.Select(a => new AssetClassBase(a, AssetTypeName)));
 
-                if (successCallback != null)
-                {
-                    successCallback(list);
-                }
-                else
-                {
-                    OnSuccess(list);
-                }
-            }
-            catch (Exception exception)
-            {
-                if (OnError != null)
-                {
-                    OnError(exception);
-                }
-                else
-                {
-                    throw;
-                }
-            }
+				if (list.Count == 0)
+				{
+					OnEmptyResults?.Invoke();
+				}
 
-            return this;
-        }
-    }
+				if (successCallback != null)
+				{
+					successCallback(list);
+				}
+				else
+				{
+					OnSuccess(list);
+				}
+			}
+			catch (Exception exception)
+			{
+				if (OnError != null)
+				{
+					OnError(exception);
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return this;
+		}
+	}
 }
