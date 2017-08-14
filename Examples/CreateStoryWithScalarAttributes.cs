63b80b77656c8436d6bfe5dc4b3b686af88b8bb6
Josh Gough <jsgough@gmail.com>
2017-08-14 13:24:26 -0400

    Create CreateStoryWithScalarAttributes.cs

---
+++ Examples/CreateStoryWithScalarAttributes.cs
@@ -0,0 +1,34 @@
+using VersionOne.SDK.APIClient;
+using static System.Console;
+
+namespace Examples
+{
+	public class CreateStoryWithScalarAttributesVNext
+	{
+		string instanceUrl = "https://www16.v1host.com/api-examples";
+		string accessToken = "1.bndNO51GiliELZu1bbQdq3omgRI=";
+
+		static void Main()
+		{
+			var example = new CreateStoryWithScalarAttributesVNext();
+			example.Execute();
+			WriteLine("Press any key to exit...");
+			ReadKey();
+		}
+
+		public void Execute()
+		{
+			var v1 = V1Connector
+				.WithInstanceUrl(instanceUrl)
+				.WithUserAgentHeader("Examples", "0.1")
+				.WithAccessToken(accessToken);
+
+			dynamic newStory = v1.Create("Story", new {
+				Scope = "Scope: 1015",
+				Name = $"Test Story Scope:1015 Update scalar attribute"
+			});
+
+      // TODO: what should newStory contain now?
+		}
+	}
+}
