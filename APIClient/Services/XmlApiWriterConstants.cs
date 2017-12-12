47c648b705dfcbf9202ac5a5909694f82bc1ffd1
Josh Gough <jsgough@gmail.com>
2017-12-11 23:52:50 -0500

    S-64245 Tabs, constants, killed dead code, tightened conditionals

---
+++ APIClient/Services/XmlApiWriterConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace VersionOne.SDK.APIClient
+{
+    public static class XmlApiWriterConstants
+    {
+        public static string Act = "act";
+        public static string ActSet = "set";
+        public static string ActAdd = "add";
+        public static string ActRemove = "remove";
+        public static string RelationElement = "Relation";
+        public static string AssetElement = "Asset";
+        public static string IdAttribute = "id";
+        public static string AttributeElement = "Attribute";
+        public static string NameAttribute = "name";
+        public static string IdRefAttribute = "idref";
+        public static string ValueElement = "Value";
+        public static string DateTimeShortFormat = "yyyy-MM-dd";
+        public static string DateTimeLongFormat = "yyyy-MM-ddTHH:mm:ss.fff";
+    }
+}
