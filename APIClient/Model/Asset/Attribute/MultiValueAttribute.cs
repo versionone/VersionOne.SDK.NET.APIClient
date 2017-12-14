562be409f2bd6c654b05e57f84d2e2780d98d3b1
Josh Gough <jsgough@gmail.com>
2017-12-14 11:36:33 -0500

    S-64245 Prevent duplication of Adds and Removes from being sent to server
    This doesn't impact the server, but it was producing duplicate values
    in the client.

--- APIClient/Model/Asset/Attribute/MultiValueAttribute.cs
+++ APIClient/Model/Asset/Attribute/MultiValueAttribute.cs
@@ -1,15 +1,29 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace VersionOne.SDK.APIClient
 {
     // TODO see if we can stop using old school collections here
     internal class MultiValueAttribute : Attribute
     {
-        private ArrayList values = new ArrayList();
-        private ArrayList addedValues;
-        private ArrayList removedValues;
-        private ArrayList newValues;
+        private class UniqueSet : HashSet<object>
+        {
+            public UniqueSet()
+            {
+            }
+
+            public UniqueSet(IEnumerable sourceSet)
+            {
+                foreach (var item in sourceSet) Add(item);
+            }
+        }
+
+        private UniqueSet _values = new UniqueSet();
+        private UniqueSet _addedValues;
+        private UniqueSet _removedValues;
+        private UniqueSet _newValues;
 
         internal MultiValueAttribute(IAttributeDefinition def, Asset asset) : base(def, asset) { }
 
@@ -17,16 +31,8 @@ namespace VersionOne.SDK.APIClient
         {
             get
             {
-                if (values.Count == 0)
-                {
-                    return Definition.Coerce(null);
-                }
-
-                if (values.Count == 1)
-                {
-                    return values[0];
-                }
-
+                if (_values.Count == 0) return Definition.Coerce(null);
+                if (_values.Count == 1) return _values.First();
                 throw new ApplicationException("Attribute contains multiple values: " + Definition.Token);
             }
         }
@@ -35,80 +41,42 @@ namespace VersionOne.SDK.APIClient
         {
             get
             {
-                if (newValues == null)
-                {
-                    return null;
-                }
-
-                if (newValues.Count == 0)
-                {
-                    return Definition.Coerce(null);
-                }
-
-                if (newValues.Count == 1)
-                {
-                    return newValues[0];
-                }
-
+                if (_newValues == null) return null;
+                if (_newValues.Count == 0) return Definition.Coerce(null);
+                if (_newValues.Count == 1) return _newValues.First();
                 throw new ApplicationException("Attribute contains multiple values: " + Definition.Token);
             }
         }
 
-        public override IEnumerable OriginalValues
-        {
-            get { return values; }
-        }
+        public override IEnumerable OriginalValues => _values;
 
-        public override IEnumerable NewValues
-        {
-            get { return newValues; }
-        }
+        public override IEnumerable NewValues => _newValues;
 
-        public override IEnumerable AddedValues
-        {
-            get { return addedValues; }
-        }
+        public override IEnumerable AddedValues => _addedValues;
 
-        public override IEnumerable RemovedValues
-        {
-            get { return removedValues; }
-        }
+        public override IEnumerable RemovedValues => _removedValues;
 
-        public override bool HasChanged
-        {
-            get { return (newValues != null); }
-        }
+        public override bool HasChanged => (_newValues != null);
 
-        internal override void SetValue(object value)
-        {
-            throw new ApplicationException("Cannot set value on a multi-value attribute: " + Definition.Token);
-        }
+        internal override void SetValue(object value) => throw new ApplicationException("Cannot set value on a multi-value attribute: " + Definition.Token);
 
-        internal override void ForceValue(object value)
-        {
-            throw new ApplicationException("Cannot force value on a multi-value attribute: " + Definition.Token);
-        }
+        internal override void ForceValue(object value) => throw new ApplicationException("Cannot force value on a multi-value attribute: " + Definition.Token);
+
+        private bool ContainsValue(object value) => _values.Contains(value) || _addedValues.Contains(value);
 
         internal override void AddValue(object value)
         {
             CheckReadOnly();
             value = Definition.Coerce(value);
             CheckNull(value);
-
             EnsureNewValues();
-            newValues.Add(value);
+            EnsureAddedValues();
 
-            if (addedValues == null)
-            {
-                addedValues = new ArrayList();
-            }
+            if (ContainsValue(value)) return;
 
-            addedValues.Add(value);
-
-            if (removedValues != null)
-            {
-                removedValues.Remove(value);
-            }
+            _newValues.Add(value);
+            _addedValues.Add(value);
+            _removedValues?.Remove(value);
         }
 
         internal override void RemoveValue(object value)
@@ -117,46 +85,30 @@ namespace VersionOne.SDK.APIClient
             value = Definition.Coerce(value);
 
             EnsureNewValues();
-            newValues.Remove(value);
+            _newValues.Remove(value);
 
-            if (removedValues == null)
-            {
-                removedValues = new ArrayList();
-            }
+            EnsureRemovedValues();
 
-            removedValues.Add(value);
+            _removedValues.Add(value);
 
-            if (addedValues != null)
-            {
-                addedValues.Remove(value);
-            }
+            _addedValues?.Remove(value);
         }
 
-        private void EnsureNewValues()
-        {
-            if (newValues == null)
-            {
-                newValues = new ArrayList(values);
-            }
-        }
+        private void EnsureNewValues() => _newValues = _newValues ?? new UniqueSet(_values);
+        private void EnsureAddedValues() => _addedValues = _addedValues ?? new UniqueSet();
+        private void EnsureRemovedValues() => _removedValues = _removedValues ?? new UniqueSet();
 
         public override void AcceptChanges()
         {
             if (HasChanged)
             {
-                values = newValues;
-                newValues = addedValues = removedValues = null;
+                _values = _newValues;
+                _newValues = _addedValues = _removedValues = null;
             }
         }
 
-        public override void RejectChanges()
-        {
-            newValues = addedValues = removedValues = null;
-        }
+        public override void RejectChanges() => _newValues = _addedValues = _removedValues = null;
 
-        internal override void LoadValue(object value)
-        {
-            values.Add(Definition.Coerce(value));
-        }
+        internal override void LoadValue(object value) => _values.Add(Definition.Coerce(value));
     }
 }
--- Examples/TaggedWith.cs
+++ Examples/TaggedWith.cs
@@ -1,61 +1,75 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using VersionOne.SDK.APIClient;
 using static System.Console;
 
-	public class TaggedWith
-	{
-		string instanceUrl = "http://localhost/VersionOne.Web";
-		string username = "admin";
-		string password = "admin";
+public class TaggedWith
+{
+    string instanceUrl = "http://localhost/VersionOne.Web";
+    string username = "admin";
+    string password = "admin";
 
-		private void Execute()
-		{
-			var connector = V1Connector
-				  .WithInstanceUrl(instanceUrl)
-				  .WithUserAgentHeader("Examples", "0.1")
-				  .WithUsernameAndPassword(username, password)
-				  .Build();
+    private void Execute()
+    {
+        var connector = V1Connector
+            .WithInstanceUrl(instanceUrl)
+            .WithUserAgentHeader("Examples", "0.1")
+            .WithUsernameAndPassword(username, password)
+            .Build();
 
-			var services = new Services(connector);
+        var services = new Services(connector);
 
-			var storyType = services.Meta.GetAssetType("Story");
+        var storyType = services.Meta.GetAssetType("Story");
 
-			var query = new Query(storyType);
+        var query = new Query(storyType);
 
-			var nameAttribute = storyType.GetAttributeDefinition("Name");
-			var idAttribute = storyType.GetAttributeDefinition("ID");
-			var taggedWithAttribute = storyType.GetAttributeDefinition("TaggedWith");
+        var nameAttribute = storyType.GetAttributeDefinition("Name");
+        var idAttribute = storyType.GetAttributeDefinition("ID");
+        var taggedWithAttribute = storyType.GetAttributeDefinition("TaggedWith");
+        var descAttribute = storyType.GetAttributeDefinition("Description");
 
-			query.Selection.Add(nameAttribute);
-			query.Selection.Add(taggedWithAttribute);
+        query.Selection.Add(nameAttribute);
+        query.Selection.Add(taggedWithAttribute);
+        query.Selection.Add(descAttribute);
 
-			var term = new FilterTerm(idAttribute);
-			term.Equal("Story:1004");
-			query.Filter = term;
+        var term = new FilterTerm(idAttribute);
+        term.Equal("Story:1004");
+        query.Filter = term;
 
-			var result = services.Retrieve(query);
+        var result = services.Retrieve(query);
 
-			foreach (var story in result.Assets)
-			{
-				WriteLine(story.Oid.Token);
-				WriteLine(story.GetAttribute(nameAttribute).Value);
-				WriteLine(string.Join(",", story.GetAttribute(taggedWithAttribute).Values.Cast<string>()));
+        foreach (var story in result.Assets)
+        {
+            WriteLine(story.Oid.Token);
+            WriteLine(story.GetAttribute(nameAttribute).Value);
+            WriteLine("Before: " + string.Join(",", story.GetAttribute(taggedWithAttribute).Values.Cast<string>()));
+            WriteLine(story.GetAttribute(descAttribute));
 
-				story.AddAttributeValue(taggedWithAttribute, "FIRST TAG");
-				story.AddAttributeValue(taggedWithAttribute, "SECOND TAG");
-				story.RemoveAttributeValue(taggedWithAttribute, "SECOND TAG");
+            story.AddAttributeValue(taggedWithAttribute, "FIRST TAG");
+            // Try to add again:
+            story.AddAttributeValue(taggedWithAttribute, "FIRST TAG");
 
-				services.Save(story);
-				story.AcceptChanges();
-			}
-		}
+            story.AddAttributeValue(taggedWithAttribute, "SECOND TAG");
+            story.RemoveAttributeValue(taggedWithAttribute, "SECOND TAG");
 
-		public static void Main(string[] args)
-		{
-			var example = new TaggedWith();
-			example.Execute();
-			WriteLine("Press any key to exit...");
-			ReadKey();
-		}
-	}
+            story.AddAttributeValue(taggedWithAttribute, "Really actually new");
+            story.AddAttributeValue(taggedWithAttribute, "Really actually new");
+            story.AddAttributeValue(taggedWithAttribute, "Really actually new 2");
+            story.AddAttributeValue(taggedWithAttribute, "Really actually new 2");
+            story.AddAttributeValue(taggedWithAttribute, "Really actually new 2");
+
+
+            services.Save(story);
+            story.AcceptChanges();
+            
+            WriteLine("After: " + string.Join(",", story.GetAttribute(taggedWithAttribute).Values.Cast<string>()));
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        var example = new TaggedWith();
+        example.Execute();
+        WriteLine("Press any key to exit...");
+        ReadKey();
+    }
+}
